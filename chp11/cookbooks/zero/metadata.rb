23c2ea376c4497250e58c338a75e010ec1734b0c
Alan <begleyalan@gmail.com>
2015-03-14 20:09:37 +0000

    Chp11 Chef Zero

---
+++ chp11/cookbooks/zero/metadata.rb
@@ -0,0 +1,8 @@
+name             'zero'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures zero'
+long_description 'Installs/Configures zero'
+version          '0.1.0'
+
