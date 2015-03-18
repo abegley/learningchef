32686b8ea5523a371e2782406d624811c0b2d9fa
Alan <begleyalan@gmail.com>
2015-03-18 21:36:19 +0000

    Adding chp15

---
+++ chp15/chef-zero/cookbooks/apache/metadata.rb
@@ -0,0 +1,8 @@
+name             'apache'
+maintainer       'Alan Begley'
+maintainer_email 'begleyalan@gmail.com'
+license          'MIT'
+description      'Installs/Configures apache'
+long_description 'Installs/Configures apache'
+version          '0.2.0'
+
